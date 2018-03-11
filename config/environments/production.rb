9a27a951da18689547995fdb08ae157b6c6d7e27
Mohd Sameer <mohdsameer1310@gmail.com>
2018-03-11 20:42:45 +0530

    Mail settings and secret key

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,6 +89,8 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
+  config.action_mailer.default_url_options = { host: '165.227.35.89'}
+
   ActionMailer::Base.smtp_settings = {
     :address              => 'smtp.sendgrid.net',
     :port                 => 587,
