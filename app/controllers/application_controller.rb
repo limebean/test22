97e3e46db97d1a1f42f3796bad0dc4e63fa73f47
Mohd Sameer <mohdsameer1310@gmail.com>
2018-03-12 14:27:24 +0530

    redirect to back page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,15 +1,29 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
+  after_action :store_location
 
   protected
     def after_sign_in_path_for(_resource)
+      return_path = session["user_return_to"]
+      session.delete("user_return_to")
       if current_user.admin?
-        admin_dashboard_path
+        return_path || admin_dashboard_path
       elsif current_user.teacher?
-        dashboard_path
+        return_path || dashboard_path
       elsif current_user.parent?
-        parents_dashboard_path
+        return_path || parents_dashboard_path
+      end
+    end
+
+    def store_location
+      # store last url - this is needed for post-login redirect to whatever the user last visited.
+      if (request.fullpath != "/users/sign_in" &&
+          request.fullpath != "/users/sign_up" &&
+          request.fullpath != "/users/password" &&
+          request.fullpath != "/users/sign_out" &&
+          !request.xhr?) # don't store ajax calls
+        session["user_return_to"] = request.fullpath
       end
     end
 
