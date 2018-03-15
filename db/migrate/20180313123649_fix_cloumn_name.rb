89d1c0a06e84b3aa0f1e09de4cc129d70b7957f6
Mohd Sameer <mohdsameer1310@gmail.com>
2018-03-15 11:02:05 +0530

    fix issue of db migration

--- db/migrate/20180313123649_fix_cloumn_name.rb
+++
@@ -1,6 +0,0 @@
-class FixCloumnName < ActiveRecord::Migration[5.1]
-  def change
-    rename_column :parent_profiles, :child_name, :parent_profile_child_name
-    rename_column :parent_profiles, :child_date_of_birth, :parent_profile_child_date_of_birth
-  end
-end
--- db/migrate/20180313132628_remove_column_from_parent_profiles.rb
+++ db/migrate/20180313132628_remove_column_from_parent_profiles.rb
@@ -1,5 +1,5 @@
 class RemoveColumnFromParentProfiles < ActiveRecord::Migration[5.1]
   def change
-  	remove_column :parent_profiles, :parent_needs
+  	remove_column :parent_profiles, :parent_needs, :string
   end
 end
