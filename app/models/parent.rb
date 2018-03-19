3e4cbacade199dcf761d07ec1da1d0d090906232
Mohd Sameer <mohdsameer1310@gmail.com>
2018-03-19 18:13:40 +0530

    remove fields

--- app/models/parent.rb
+++ app/models/parent.rb
@@ -1,5 +1,5 @@
 class Parent < User
   has_one :parent_profile, foreign_key: :parent_id, dependent: :destroy
-	has_many :children, dependent: :destroy
+  has_many :children, dependent: :destroy
   has_many :favourites, dependent: :destroy
 end
