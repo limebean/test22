2273bc8458c84d6aa5cb18d151071c431a81f0c9
Mohd Sameer <mohdsameer1310@gmail.com>
2018-03-22 10:26:26 +0530

    enroll_finish

--- app/controllers/enrollments_controller.rb
+++ app/controllers/enrollments_controller.rb
@@ -6,7 +6,7 @@ class EnrollmentsController < ApplicationController
 	def create
 		@enroll_app = Enrollment.new(permitted_enrollment_params)
     if @enroll_app.save
-      redirect_to enroll_application_teacher_path(params[:child][:teacher_profile_id]), notice: "success"
+      redirect_to enrollment_path(@enroll_app), notice: "success"
     else
       redirect_to partner_path, alert: "Something went wrong! Please try again."
     end
@@ -19,17 +19,11 @@ class EnrollmentsController < ApplicationController
 
   def permitted_enrollment_params
     params.require(:enrollment).permit(
-      :teacher_profile_id,
-      :full_name,
-      :age,
-      :care_by,
-      :date_of_birth,
-      :parent_id,
-      :place_of_birth,
-      :gender,
-      :allergies,
-      :medication,
-      :medical_condition
+      :child_id,
+      :teacher_id,
+      :start_date,
+      :weekdays_and_time,
+      :other_comments
     )
   end
 end
